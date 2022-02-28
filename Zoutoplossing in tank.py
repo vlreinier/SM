0a31dd2ed0a624f8f586fdb39a0e275e3494ffec
vlreinier <43336873+vlreinier@users.noreply.github.com>
2022-02-28 21:56:29 +0100

    Update Zoutoplossing in tank.py

--- Zoutoplossing in tank.py
+++ Zoutoplossing in tank.py
@@ -1,34 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def tank_concentrations(steps=120, liters=1000, inflow=6, outflow=6, concentration_inflow=0.1):
+def tank_concentrations(steps=1000, liters=1000, inflow=6, outflow=6, concentration_inflow=0.1):
     concentrations = []
     current_concentration = 0
+    current_volume = liters
     concentrations.append(current_concentration)
-    for step in range(steps):
-        in_tank = inflow / liters * concentration_inflow # salt concentration in flow for this step
-        out_tank = outflow / liters * current_concentration # salt concentration out flow for this step
-        current_concentration = current_concentration + in_tank - out_tank
+
+    for _ in range(steps):
+        # Save temp volume to change tank concentration with added new volume
+        temp_volume = current_volume
+
+        # Change volume according to in and out flow
+        current_volume += inflow - outflow
+
+        # Salt concentration going into the tank with new volume
+        concentration_in = concentration_inflow * inflow / current_volume
+
+        # Salt concentration going out of the tank with new volume
+        concentration_out = current_concentration * outflow / current_volume
+
+        # New tank salt concentration for tank with new volume
+        new_tank_concentration = current_concentration * temp_volume / current_volume
+
+        # New tank salt concentration for tank after concentration in and out tank with new volume
+        current_concentration = new_tank_concentration + concentration_in - concentration_out
+
         concentrations.append(current_concentration)
     return concentrations
 
-fig, axes = plt.subplots(nrows=1, ncols=2)
-
-# Outflow 6
 concentrations_6 = tank_concentrations(outflow=6)
-axes[0].title.set_text("Tank outflow: 6 L/min")
-axes[0].plot(range(len(concentrations_6)), concentrations_6)
-axes[0].set_yticks(np.arange(0, 0.06, step=.005))
-axes[0].set_xlabel("Steps")
-axes[0].set_ylabel("Salt concentration")
-
-# Outflow 5 -> salt concentration in tank remains higher since in and outflow are not identical
 concentrations_5 = tank_concentrations(outflow=5)
-axes[1].title.set_text("Tank outflow: 5 L/min")
-axes[1].plot(range(len(concentrations_5)), concentrations_5)
-axes[1].set_yticks(np.arange(0, 0.06, step=.005))
-axes[1].set_xlabel("Steps")
-axes[1].set_ylabel("Salt concentration")
 
-plt.tight_layout()
+plt.title("Salt concentration over time")
+plt.plot(range(len(concentrations_6)), concentrations_6, label="6 L/min")
+plt.plot(range(len(concentrations_5)), concentrations_5, label="5 L/min")
+plt.yticks(np.arange(0, 0.11, step=.005))
+plt.xlabel("Steps")
+plt.ylabel("Salt concentration")
+plt.legend()
 plt.show()
